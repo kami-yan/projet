0855f66f8143c75da354085a9d41a09ded9de871
kami-yan <151518355+kami-yan@users.noreply.github.com>
2023-11-27 11:41:40 +0100

    Add files via upload

---
+++ fichier_matlab/files/animation_script.m
@@ -0,0 +1,10 @@
+ x_data = out.x.Data;
+ y_data = out.y.Data;
+ theta_data = out.theta.Data;
+ delta1_data = out.delta1.Data;
+ delta2_data = out.delta2.Data;
+
+ n = size (x_data);
+ for i = 1:n
+     visual( x_data(i),y_data(i),theta_data(i),delta1_data(i),delta2_data(i));
+ end;
---
+++ fichier_matlab/files/plotting.m
@@ -0,0 +1,90 @@
+
+
+% If the data is in timeseries format, you need to extract the data like this:
+ x_data = out.x.Data;
+ y_data = out.y.Data;
+
+% If the data is in timeseries format, you need to extract the data like this:
+ rx_data = out.rx.Data;
+ ry_data = out.ry.Data;
+
+% If the data is in timeseries format, you need to extract the data like this:
+ ex_data = out.ex.Data;
+ ey_data = out.ey.Data;
+
+
+% If the data is in timeseries format, you need to extract the data like this:
+ ex_relative_data = out.ex_robot.Data;
+ ey_relative_data = out.ey_robot.Data;
+
+
+% Plot the XY data
+figure;
+plot(rx_data, ry_data);
+xlabel('X');
+ylabel('Y');
+title('target points');
+grid on;
+
+% Plot the XY data
+figure;
+plot(x_data, y_data);
+xlabel('X');
+ylabel('Y');
+title('la trajectoire de p du robot ');
+grid on;
+
+% Plot the XY data
+figure;
+plot(rx_data, ry_data);
+xlabel('X');
+ylabel('Y');
+title('target points');
+grid on;
+
+% Plot the XY data
+hold on;
+plot(x_data, y_data);
+xlabel('X');
+ylabel('Y');
+title('la trajectoire de p du robot ');
+grid on;
+
+legend('target','trajectoire du robot')
+
+% Plot the error data
+figure;
+plot(out.tout, ey_data);
+xlabel('X');
+ylabel('Y');
+title("l'erreur selon y dans le repere du laboratoire");
+grid on;
+
+% Plot the error data
+hold on;
+plot(out.tout, ex_data);
+xlabel('X');
+ylabel('Y');
+title("l'erreur dans le repere du laboratoire");
+grid on;
+
+legend("l'erreur absolue selon x","l'erreur absolue selon y")
+
+
+% Plot the error data
+figure;
+plot(out.tout, ey_relative_data);
+xlabel('X');
+ylabel('Y');
+title("l'erreur selon x dans le repere du robot");
+grid on;
+
+% Plot the error data
+hold on;
+plot(out.tout, ex_relative_data);
+xlabel('X');
+ylabel('Y');
+title("l'erreur dans le repere du robot");
+grid on;
+
+legend("l'erreur selon x","l'erreur selon y")
