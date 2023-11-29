3358739755dc65e43b6fd08e969e83d22515969d
kami-yan <151518355+kami-yan@users.noreply.github.com>
2023-11-29 10:22:35 +0100

    Update animation_script.m

--- fichier_matlab/files/animation_script.m
+++ fichier_matlab/files/animation_script.m
@@ -3,9 +3,17 @@
  theta_data = out.theta.Data;
  delta1_data = out.delta1.Data;
  delta2_data = out.delta2.Data;
+ rx_data = out.rx.Data;
+ ry_data = out.ry.Data;
 
  n = size (x_data);
  for i = 1:n
      clf;
+
+     plot(rx_data, ry_data);
+     xlabel('X');
+     ylabel('Y');
+     title('target points');
+     
      visual( x_data(i),y_data(i),theta_data(i),delta1_data(i),delta2_data(i));
  end;
