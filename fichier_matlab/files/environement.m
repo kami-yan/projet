d4456de0e0bc7b006db02584c618ddbe38c537c6
elbachaayy <144458158+elbachaayy@users.noreply.github.com>
2023-12-15 23:52:57 +0100

    Update environement.m

--- fichier_matlab/files/environement.m
+++ fichier_matlab/files/environement.m
@@ -1,1 +1,3 @@
-yes
+
+centres_obs = [4, 10; 3, 2; 9,12;5,16;15,15;15,2;7,6;13,8]; %une matrice contenant les coordonnées des centres des cercles, chaque ligne représentant les coordonnées [x, y] d'un centre
+rayon_obs = [0.5, 0.4,1,0.2,1,0.7,0.8,0.6];  %un vecteur contenant les rayons des cercles
